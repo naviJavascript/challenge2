9d01f1ee5b7f388e968f989dbc2e278f43c07152
naviJavascript <88846131+naviJavascript@users.noreply.github.com>
2023-08-28 13:54:52 -0700

    serving index.html

--- myApp.js
+++ myApp.js
@@ -2,12 +2,19 @@ let express = require('express');
 let app = express();
 
 console.log('hello world');
+console.log(__dirname);
 
+const absolutePath = __dirname + '/views/index.html';
 
+/*
 app.get('/', (req, res) => {
     res.send('Hello Express');
 });
+*/
 
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+});
 
 
 
