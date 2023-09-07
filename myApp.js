1a703f03ef73ab717951965e61b240a388b20db3
naviJavascript <88846131+naviJavascript@users.noreply.github.com>
2023-09-07 13:11:02 -0700

    bodyparser

--- myApp.js
+++ myApp.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
-
-console.log('hello world');
-// console.log(__dirname);
 
 const indexPath = __dirname + '/views/index.html';
 const public = __dirname + '/public';
+
+// con el middleware express.static se pueden servir archivos estaticos
+// la ventaja de usar este middeleware es que podemos servir toda una carpeta
+// en este caso la carpeta public donde estan los archivos estaticos, como hojas de
+// estilo y js
+app.use('/public', express.static(public));
+//------------------------------------------------
+
+// middleware bodyParser sirve para analizar el cuerpo de una solicitud post
+// y se debe montar antes de las rutas que dependan del mismo
+app.use(bodyParser.urlencoded({extended: false}));
 
 /*
 app.get('/', (req, res) => {
@@ -51,6 +60,11 @@ app.get('/:word/echo', (req,res) => {
     res.send({echo: word});
 });
 
+
+// middleware bodyParser sirve para analizar el cuerpo de una solicitud post
+// y se debe montar antes de las rutas que dependan del mismo
+
+app.use('/name', bodyParser.urlencoded({extended: false}));
 
 // parametros de consulta o parametros query
 app.route('/name').get((req, res) => {
