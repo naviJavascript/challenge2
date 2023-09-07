162bb739f6dd4ef1581ad2f6845feded98ff5d71
naviJavascript <88846131+naviJavascript@users.noreply.github.com>
2023-09-07 14:37:05 -0700

    send post

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@ const public = __dirname + '/public';
 app.use('/public', express.static(public));
 //------------------------------------------------
 
-// middleware bodyParser sirve para analizar el cuerpo de una solicitud post
+// middleware bodyParser sirve para analizar el cuerpo de una solicitud o peticion
 // y se debe montar antes de las rutas que dependan del mismo
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -33,11 +33,7 @@ app.get('/', (req, res) => {
     res.sendFile(indexPath);
 });
 
-// con el middleware express.static se pueden servir archivos estaticos
-// la ventaja de usar este middeleware es que podemos servir toda una carpeta
-// en este caso la carpeta public donde estan los archivos estaticos, como hojas de
-// estilo y js
-app.use('/public', express.static(public));
+
 
 app.get('/json', (req,  res) => {
     let response = 'Hello json';
@@ -72,7 +68,10 @@ app.route('/name').get((req, res) => {
     let last = req.query.last;
     res.send({name: first + " " + last});
 }).post((req, res) => {
-
+  
+    let first = req.body.first;
+    let last =  req.body.last;
+    res.send({name: first + " " + last});
 });
 
 
